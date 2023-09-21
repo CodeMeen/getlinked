56e485e9dcf4aead0569df1a2dbcaf7aca2ba4b1
Ameen <alameenolumomi@yahoo.com>
2023-09-21 20:07:46 +0100

    commit

---
+++ src/pages/home.js
@@ -0,0 +1,560 @@
+import '../App.css'
+
+
+function Home(){
+
+    return (
+        <div className="body">
+
+          <div className='head'>
+
+            <div className='backlayer'>
+
+                <div className='gradientOne' ></div>
+                <div className='gradientTwo' ></div>
+
+                <div className='metrix'></div>
+
+                <div className='st starOne'></div>
+                <div className='st starTwo'></div>
+                <div className='st starThree'></div>
+
+                <span className='textgh'>
+                <div>Igniting a Revolution in HR Innovation</div>
+                <div className='bentline'></div>
+                </span>
+                
+
+            </div>
+
+            <div className="header">
+
+                <div className='textlogo'>
+                    <span className='whitetext'>get</span><span className='colortext'>linked</span>
+                </div>
+
+                <div className='header_menu'>
+                   <div className='each_menu'>Timeline</div>
+                   <div className='each_menu'>Overview</div>
+                   <div className='each_menu'>FAQs</div>
+                   <div className='each_menu'>Contact</div>
+
+                   <button className='register_btn'>Register</button>
+                </div>
+
+            </div>
+
+
+           
+
+                <span className='containerOne'>
+
+
+                     <div className="relative_box">
+                     <div className='bulb'></div>
+                     <div className='gtext'>getlinkedTech</div>
+                     </div>
+                   
+                    <div className="flex_cont"> <span className='gtext'>Hackathon</span><span className='gtext colortext'> 1.0</span> <div className='chain_icon'></div> <div className='fire_icon'></div> </div>
+
+                    <span className='participate_text'>Participate in getlinked tech Hackathon 2023 stand <br/>
+                    a chance to win a Big prize</span>
+
+                    <button className='register_btn'>Register</button>
+
+
+                <div className='timer_container'>
+                    <div>
+                        <span className='oo'>00</span>
+                        <span className='oo_t'>H</span>
+                    </div>
+                    <div>
+                        <span className='oo'>00</span>
+                        <span className='oo_t'>M</span>
+                    </div>
+                    <div>
+                         <span className='oo'>00</span>
+                        <span className='oo_t'>S</span>
+                    </div>
+                </div>
+                     
+                </span>
+
+                <div className='containerTwo'>
+
+
+                    <div className='relative_box'>
+                       <div className='guy_in_googles'></div>
+                       <div className='circle_sparkles'></div>
+                    </div>
+
+                    
+
+
+                    
+                </div>
+
+            
+
+        
+
+           </div>
+
+
+           <div className='bodyOne'>
+            <div className='backlayer'>
+                <div className='st starOne'></div>
+                <div className='rough_arrow'></div>
+                <div className='st starTwo'></div>
+
+               
+
+            </div>
+
+            <div className='boxContainer'>
+                <div className='icon thebigidea'></div>
+
+                <div className='textspace'>
+                   
+
+                   <div>
+                    <span className='gtext'>Introduction to getlinked</span><br/>
+                    <span className='gtext colortext'>techHackathon 1.0</span>
+                    
+                   
+                    <br/><br/>
+
+                    
+                    <div className='rtext'>
+                    Our tech hackathon is a melting pot of visionaries, and its purpose is as
+clear as day: to shape the future. Whether you're a coding genius, a 
+design maverick, or a concept wizard, you'll have the chance to transform 
+your ideas into reality. Solving real-world problems, pushing the boundaries
+of technology, and creating solutions that can change the world,
+that's what we're all about!
+                    </div>
+                    </div>
+                    
+
+                </div>
+            </div>
+
+           </div>
+
+
+           <div className='bodyTwo'>
+            <div className='backlayer'>
+                <div className='st starOne'></div>
+
+                <div className='st starTwo'></div>
+
+                <div className="gradientOne"></div>
+
+                <div className="gradientTwo"></div>
+
+            </div>
+
+            <div className='boxContainer'>
+            <div className='textspace'>
+                   
+
+                   <div>
+                    <span className='gtext'>Rules and</span><br/>
+                    <span className='gtext colortext'>Guidelines</span>
+                    
+                   
+                    <br/><br/>
+
+                    
+                    <div className='rtext'>
+                    Our tech hackathon is a melting pot of visionaries, and its purpose is as
+clear as day: to shape the future. Whether you're a coding genius, a 
+design maverick, or a concept wizard, you'll have the chance to transform 
+your ideas into reality. Solving real-world problems, pushing the boundaries
+of technology, and creating solutions that can change the world,
+that's what we're all about!
+                    </div>
+                    </div>
+                    
+
+                </div>
+
+                <div className='icon sitting'></div>
+
+            
+            </div>
+
+           </div>
+
+
+           <div className='bodyThree'>
+
+            <div className='backlayer'>
+                <div className='st starOne'></div>
+
+                <div className='st starTwo'></div>
+
+                <div className='st starThree'></div>
+
+                <div className="gradientOne"></div>
+
+                <div className="gradientTwo"></div>
+
+            </div>
+
+            <div className='boxContainer'>
+
+            <div className='icon people'></div>
+
+
+            <div className='textspace'>
+                   
+
+                   <div>
+                    <span className='gtext'>Judging Criteria</span><br/>
+                    <span className='gtext colortext'>Key attributes</span>
+                    
+                   
+                    <br/><br/>
+
+                     <div className='textgrid'>
+
+                     <div className='rtext'>
+                    <span className='colortexttwo'>Innovation and Creativity:</span> Evaluate the uniqueness and creativity of the
+solution. Consider whether it addresses a real-world problem in a novel 
+way or introduces innovative features.
+                    </div>
+
+
+                    <div className='rtext'>
+                    <span className='colortexttwo'>Functionality:</span> Assess how well the solution works. Does it perform its 
+intended functions effectively and without major issues? Judges would
+consider the completeness and robustness of the solution.
+                    </div>
+
+                    <div className='rtext'>
+                    <span className='colortexttwo'>Impact and Relevance:</span> Determine the potential impact of the solution 
+in the real world. Does it address a significant problem, and is it relevant 
+to the target audience? Judges would assess the potential social, 
+economic, or environmental benefits.
+                    </div>
+
+                    <div className='rtext'>
+                    <span className='colortexttwo'>Technical Complexity:</span> Evaluate the technical sophistication of the solution. 
+Judges would consider the complexity of the code, the use of advanced 
+technologies or algorithms, and the scalability of the solution.
+                    </div>
+
+
+                    <div className='rtext'>
+                    <span className='colortexttwo'>Adherence to Hackathon Rules:</span> Judges will Ensure that the team adhered 
+to the rules and guidelines of the hackathon, including deadlines, use of 
+specific technologies or APIs, and any other competition-specific requirements.
+                    </div>
+
+
+
+
+
+
+
+
+                     </div>
+                    
+                    
+                    </div>
+                    
+
+                </div>
+
+              
+
+            
+            </div>
+
+           </div>
+
+
+           <div className='bodyFour'>
+            <div className='backlayer'>
+
+                <div className='st starOne'></div>
+                <div className='st starTwo'></div>
+                <div className='st starThree'></div>
+                <div className='st starFour'></div>
+                <div className='st starFive'></div>
+
+               
+
+            </div>
+
+            <div className='boxContainer'>
+            <div className='textspace'>
+                   
+
+                   <div>
+                    <span className='gtext'>Frequently Ask</span><br/>
+                    <span className='gtext colortext'>Question</span>
+
+                    <br/><br/>
+                    <div className='rtext'>
+                    We got answers to the questions that you might
+                    want to ask about <b>getlinked Hackathon 1.0</b>
+                    </div>
+
+                    <div className='accordion'>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>Can I work on a project I started before the hackathon?</span>
+                        <span className='o_text'>+</span>
+                       </div>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>What happens if I need help during the hackathon?</span>
+                        <span className='o_text'>+</span>
+                       </div>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>What happens if I don't have an idea for a project?</span>
+                        <span className='o_text'>+</span>
+                       </div>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>Can I join a team or do I have to come with one?</span>
+                        <span className='o_text'>+</span>
+                       </div>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>What happens after the hackathon ends</span>
+                        <span className='o_text'>+</span>
+                       </div>
+
+                       <div className='each_accd'>
+                        <span className='c_text'>Can I work on a project I started before the hackathon?</span>
+                        <span className='o_text'>+</span>
+                       </div>
+                    </div>
+
+
+                    </div>
+                    
+
+                </div>
+
+                <div className='icon thinking'></div>
+
+            
+            </div>
+
+           </div>
+
+           <div className='bodyFive'>
+
+           <div className='backlayer'>
+
+<div className='st starOne'></div>
+<div className='st starTwo'></div>
+<div className='st starThree'>
+    
+</div>
+
+
+
+
+            </div>
+
+            <div className="divy">
+
+            <div className='bodytitle'>
+                <span className='gtext'>Timeline</span>
+                <br></br>
+                <span className='rtext'>Here is the breakdown of the time we anticipate <br/>
+using for the upcoming event.</span>
+            </div>
+
+
+            <div className='de_grid'>
+
+                
+                {/* One */}
+
+                <div className='textshii one'>
+                    <span className='gtext colortext'>Hackathon Announcement</span>
+                    <br/><br/>
+                    <span className='rtext'>
+                        The getlinked tech hackathon 1.0 is formally announced
+                     to the general public and teams begin to get ready to register
+                    </span>
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ firstcirc'>1</div>
+                   
+
+                </div>  
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>November 18, 2023</span>
+                </div>
+
+
+
+                  {/* Two */}
+
+
+
+                <div className='textshii one'>
+                    <span className='gtext colortext'>November 18, 2023</span>
+                    
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ'>2</div>
+                   
+
+                </div>  
+
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>Teams Registration begins</span>
+                <br/><br/>
+                    <span className='rtext'>
+                    Interested teams can now show their interest in the
+getlinked tech hackathon 1.0 2023 by proceeding to register
+                    </span>
+                </div>
+
+
+
+                {/* Three */}
+
+
+                <div className='textshii one'>
+                    <span className='gtext colortext'>Teams Registration ends</span>
+                    <br/><br/>
+                    <span className='rtext'>
+                    Interested Participants are no longer Allowed to
+register
+                    </span>
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ'>3</div>
+                   
+
+                </div>  
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>November 18, 2023</span>
+                </div>
+
+               {/* Four */}
+
+
+               <div className='textshii one'>
+                    <span className='gtext colortext'>November 18, 2023</span>
+                    
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ'>4</div>
+                   
+
+                </div>  
+
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>Announcement of the accepted teams
+and ideas</span>
+                <br/><br/>
+                    <span className='rtext'>
+                    All teams whom idea has been accepted into getlinked tech
+hackathon 1.0 2023 are formally announced
+                    </span>
+                </div>
+
+
+                {/* Five */}
+
+                <div className='textshii one'>
+                    <span className='gtext colortext'>Getlinked Hackathon 1.0 Offically Begins</span>
+                    <br/><br/>
+                    <span className='rtext'>
+                    Accepted teams can now proceed to build their
+ground breaking skill driven solutions
+                    </span>
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ'>5</div>
+                   
+
+                </div>  
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>November 18, 2023</span>
+                </div>
+
+
+
+                {/* Six */}
+
+
+
+
+               <div className='textshii one'>
+                    <span className='gtext colortext'>November 18, 2023</span>
+                    
+                </div>
+
+                <div className='each_line'>
+
+                   
+                    <div className='circ lastcirc'>6</div>
+                   
+
+                </div>  
+
+
+                <div className='textshii two'>
+                <span className='gtext colortext'>Demo Day</span>
+                <br/><br/>
+                    <span className='rtext'>
+                    Teams get the opportunity to pitch their projects to judges.
+The winner of the hackathon will also be announced on
+this day
+                    </span>
+                </div>
+
+
+
+
+
+            </div>
+
+
+           
+
+            </div>
+
+
+           </div>
+
+           
+
+        </div>
+    )
+
+}
+
+export default Home;
