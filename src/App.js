d1a13d4a699b4e205f1b33a9f9ccaaeded8c516e
Ameen <alameenolumomi@yahoo.com>
2023-09-24 12:36:01 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import Layout from "./pages/layout";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
 import SignUp from "./pages/signup";
+import { useEffect,useRef } from "react";
 
 function App() {
+
+
+  useEffect(()=>{
+    // variables declarations
+var i;
+var star = document.getElementsByClassName("st").length;
+
+console.log(star)
+
+for (i = 0; i < star; i++) {
+
+let y = Math.floor(Math.random() * 6);
+
+document.getElementsByClassName("st")[i].style.animationDelay = y + "s";  
+} 
+
+  },[])
+
+
   return (
     <BrowserRouter>
       <Routes>
